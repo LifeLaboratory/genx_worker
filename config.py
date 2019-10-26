0a28fbf73cf3460c85b87bdc3fc5532074f2cb32
dsoldatow <dsoldatov26@gmail.com>
2019-10-26 16:42:58 +0700

    add cert,worker,config

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 name = "First"
 host = '0.0.0.0'
-port = 8080
+port = 99999
--- models/worker.py
+++ models/worker.py
@@ -3,9 +3,12 @@ import time
 from models.cert import Cert
 import hashlib
 
+
 class Worker(BaseModel):
     def __init__(self, common_name):
         expired_time = str(int(time.time()))
-        _hash = hashlib.md5((';'.join([common_name, expired_time])).encode())
-        _cert = ';'.join[expired_time, common_name, _hash]
+        _hash = str(hashlib.md5((';'.join([common_name, expired_time])).encode()))
+        _cert = ';'.join([expired_time, common_name, _hash])
         self.cert = Cert(_cert)
+        self.queue_tasks = []
+        self.finished_tasks = []
